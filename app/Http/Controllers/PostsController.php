09a0cd1e158fb513d0c3c3b81f06c29205928d5b
Gabriel Joshua Paet <gjvpaet@gmail.com>
2016-10-30 18:16:35 +0800

    Store data in the database and add validations.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -5,6 +5,7 @@ namespace Blog\Http\Controllers;
 use Illuminate\Http\Request;
 
 use Blog\Http\Requests;
+use Blog\Post;
 
 class PostsController extends Controller
 {
@@ -36,7 +37,22 @@ class PostsController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        // validate the data
+        $this->validate($request, array(
+            'title' => 'required|max:255',
+            'body' => 'required',
+        ));
+
+        // store in the database
+        $post = new Post();
+
+        $post->title = $request->title;
+        $post->body = $request->body;
+
+        $post->save();
+
+        // redirect to another page
+        return redirect()->route('posts.show', $post->id);
     }
 
     /**
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,24 +1,29 @@
 @extends('templates.main')
 
 @section('title', '| Create New Post')
+
+@section('stylesheets')
+    {{ Html::style('css/parsley.css') }}
+@endsection
     
 @section('content')
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
             <h1>Create New Post</h1>
             <hr>
-            <form action="{{ route('posts.store') }}" method="POST">
-                <div class="form-group">
-                    <label for="title" name="title">Title:</label>
-                    <input type="text" id="title" name="title" class="form-control">
-                </div>
-                <div class="form-group">
-                    <label for="body" name="body">Post Body:</label>
-                    <textarea id="body" name="body" rows="10" class="form-control"></textarea>
-                </div>
-                <input type="submit" value="Create Post" class="btn btn-success btn-lg btn-block" style="margin-top: 20px;">
-                {{ csrf_field() }}
-            </form>
+            {!! Form::open(array('route' => 'posts.store', 'data-parsley-validate' => '')) !!}
+                {{ Form::label('title', 'Title:') }}
+                {{ Form::text('title', null, array('class' => 'form-control', 'required' => '', 'maxlength' => '255')) }}
+
+                {{ Form::label('body', 'Post Body:') }}
+                {{ Form::textarea('body', null, array('class' => 'form-control', 'required' => '')) }}
+
+                {{ Form::submit('Create Post', array('class' => 'btn btn-success btn-lg btn-block', 'style' => 'margin-top: 20px;')) }}
+            {!! Form::close() !!}
         </div>
     </div>
+@endsection
+
+@section('scripts')
+    {{ Html::script('js/parsley.min.js') }}
 @endsection
